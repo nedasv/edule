ae71063a6e2e216c85dbc733f7d61f2f315d6601
nedasv <nedasvauspas@gmail.com>
2026-02-09 15:07:14 +0000

    return json file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #[macro_use] extern crate rocket;
 
+use rocket::fs::NamedFile;
+use std::path::Path;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn get_json() -> Option<NamedFile> {
+    NamedFile::open(Path::new("data/rooms.json")).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![get_json])
 }
